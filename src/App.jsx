2ecb0d69edcf9d7bd4ef18c18bcd86b6d6632913
hristijankostadinoski <hristijan@2coders.com>
2024-10-22 09:07:54 +0200

    remove unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-// VITE_APP_API=https://api.themoviedb.org/3
-// VITE_APP_API_KEY = f67f4686e991d0dc16c641fd9507ceb3
-// API for movies: `${apiBaseUrl}/movie/popular?api_key=${apiKey}&page=${page}`
-// API for movie details: `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`
-// API for search: `${apiBaseUrl}/search/movie?api_key=${apiKey}&query=${searchedTerm}`
-
 import { Route, Routes } from "react-router-dom"
 import MovieList from "./pages/movieList/movieList"
 import Navbar from "./components/navbar/navbar"
 import MovieDetails from "./pages/movieDetails/movieDetails"
-import PageNotFound from "./pages/pageNotFound/pageNotFound"
+import PageNotFound from "./pages/PageNotFound/PageNotFound"
 
 function App() {
   return (
